b1a6d57eed724efc288ea06464470b3aae3b665e
DwayneL <dr2@ameritech.net>
2019-10-27 21:24:18 -0500

    cleaned up formatting on html. added ajax code for uv index

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ $(document).ready(function(){
     //     alert(citySearch)
     // })
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=chicago&APPID=3962360e63ed61d5bc7374f81a485e9b&units=imperial";
-    var iconURL = "http://openweathermap.org/img/wn/10d@2x.png"
+    var iconURL = "http://openweathermap.org/img/wn/04n@2x.png";
+    var indexURL = "http://api.openweathermap.org/data/2.5/uvi?appid=3962360e63ed61d5bc7374f81a485e9b&lat=41.8756&lon=-87.6245";
        
 
     $.ajax ({
@@ -17,10 +18,10 @@ $(document).ready(function(){
             var cityName = response.city.name;
             $("#nameDate").html(cityName);
 
-            var cityIcon = response.list[0].weather[0].icon;
-            $("#nameDate").append("<img src=" + iconURL +">");
+            var cityIcon = response.list[0].weather.icon;
+            $("#nameDate").append("<img src='" + iconURL +"'>");
             
-            console.log(cityIcon)
+            
 
             var cityTemp = response.list[0].main.temp;
             $("#currentTemp").html("Current Temp: "+ cityTemp + " F");
@@ -31,6 +32,10 @@ $(document).ready(function(){
             var cityWind = response.list[3].wind.speed;
             $("#windSpeed").html("Wind Speed: " + cityWind + "mph");
 
+            cityLat = response.city.coord.lat;
+            cityLon = response.city.coord.lon;
+            
+
             for(i=0; i<5; i++){
                 var highList = response.list[i].main.temp_max;
                 var lowList = response.list[i].main.temp_min;
@@ -39,6 +44,18 @@ $(document).ready(function(){
 
 
         });
+
+    $.ajax ({
+        url:indexURL,
+        method:"GET"
+    }).then(function (response2){
+        var index = response2.value
+        $("#uvIndex").html("UV Index: " + index);        
+
+    })
+
+
+    
 
        
    
