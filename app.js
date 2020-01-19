c41dd9b2beb27cb3d99e6625fa2e1eae4849b488
DwayneL <dr2@ameritech.net>
2020-01-19 17:27:21 -0600

    added functionality to search bar. app pulls info from api based on search term

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
-    // CURRENT CONDITIONS
-    // var citySearch = $("#searchBar").val();
-    // $("#searchButton").click(function(){
-    //     alert(citySearch)
-    // })
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=chicago&APPID=3962360e63ed61d5bc7374f81a485e9b&units=imperial";
-    var iconURL = "http://openweathermap.org/img/wn/04n@2x.png";
+  
+
+var searchData = $("#searchButton").click(function(){
+    var citySearch = $("#searchBar").val().trim();
+    $("#highTemp").empty() 
+    console.log(citySearch)  
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+citySearch+"&APPID=3962360e63ed61d5bc7374f81a485e9b&units=imperial";
+    
+    
     var indexURL = "http://api.openweathermap.org/data/2.5/uvi?appid=3962360e63ed61d5bc7374f81a485e9b&lat=41.8756&lon=-87.6245";
        
 
@@ -13,46 +16,52 @@ $(document).ready(function(){
         url: queryURL,
         method: "GET"
     }).then (function(response){
-         
+        var cityName = response.city.name;
+        $("#nameDate").html(cityName);
+        
+        
 
-            var cityName = response.city.name;
-            $("#nameDate").html(cityName);
+        var cityTemp = response.list[0].main.temp;
+        $("#currentTemp").html("Current Temp: "+ cityTemp + " F");
+    
+        var cityHumid = response.list[0].main.humidity;
+        $("#humidity").html("Humidity: " + cityHumid + "%");
+    
+        var cityWind = response.list[0].wind.speed;
+        $("#windSpeed").html("Wind Speed: " + cityWind + "mph");
 
-            var cityIcon = response.list[0].weather.icon;
-            $("#nameDate").append("<img src='" + iconURL +"'>");
-            
-            
+        var cityIcon = response.list[0].weather[0].icon;
+        var iconURL = "http://openweathermap.org/img/wn/"+cityIcon+"@2x.png";
+        $("#nameDate").html("<img src=" + iconURL +">");
 
-            var cityTemp = response.list[0].main.temp;
-            $("#currentTemp").html("Current Temp: "+ cityTemp + " F");
-     
-            var cityHumid = response.list[0].main.humidity;
-            $("#humidity").html("Humidity: " + cityHumid + "%");
-     
-            var cityWind = response.list[3].wind.speed;
-            $("#windSpeed").html("Wind Speed: " + cityWind + "mph");
+        console.log(iconURL)
 
-            cityLat = response.city.coord.lat;
-            cityLon = response.city.coord.lon;
-            
-
-            for(i=0; i<5; i++){
-                var highList = response.list[i].main.temp_max;
-                var lowList = response.list[i].main.temp_min;
-                $("#highTemp").append("<div>High:"+highList+" Low:"+lowList+"</div>");
-            }
+        for(i=0; i<5; i++){
+            var highList = response.list[i].main.temp_max;
+            var lowList = response.list[i].main.temp_min;
+            $("#highTemp").append("<div>High:"+highList+" Low:"+lowList+"</div>");
+        }
 
 
         });
-
-    $.ajax ({
-        url:indexURL,
-        method:"GET"
-    }).then(function (response2){
-        var index = response2.value
-        $("#uvIndex").html("UV Index: " + index);        
-
+        $.ajax ({
+            url:indexURL,
+            method:"GET"
+        }).then(function (response){
+            cityLat = response.city.coord.lat;
+            cityLon = response.city.coord.lon;
+            
+            var index = response.value
+            $("#uvIndex").html("UV Index: " + index);        
+    
+        });
     })
+    
+    
+
+    
+
+ 
 
 
     
