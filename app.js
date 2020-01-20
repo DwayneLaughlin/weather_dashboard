e91c839eea21d45bbd96f3c9529a04e83c81b6bd
DwayneL <dr2@ameritech.net>
2020-01-19 20:22:20 -0600

    search bar functionality complete. searching for a city pulls information for individual cities, including uv index

--- app.js
+++ app.js
@@ -4,14 +4,10 @@ $(document).ready(function(){
 var searchData = $("#searchButton").click(function(){
     var citySearch = $("#searchBar").val().trim();
     $("#highTemp").empty() 
-    console.log(citySearch)  
+     
 
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+citySearch+"&APPID=3962360e63ed61d5bc7374f81a485e9b&units=imperial";
     
-    
-    var indexURL = "http://api.openweathermap.org/data/2.5/uvi?appid=3962360e63ed61d5bc7374f81a485e9b&lat=41.8756&lon=-87.6245";
-       
-
     $.ajax ({
         url: queryURL,
         method: "GET"
@@ -19,8 +15,6 @@ var searchData = $("#searchButton").click(function(){
         var cityName = response.city.name;
         $("#nameDate").html(cityName);
         
-        
-
         var cityTemp = response.list[0].main.temp;
         $("#currentTemp").html("Current Temp: "+ cityTemp + " F");
     
@@ -34,29 +28,32 @@ var searchData = $("#searchButton").click(function(){
         var iconURL = "http://openweathermap.org/img/wn/"+cityIcon+"@2x.png";
         $("#nameDate").html("<img src=" + iconURL +">");
 
-        console.log(iconURL)
 
         for(i=0; i<5; i++){
             var highList = response.list[i].main.temp_max;
             var lowList = response.list[i].main.temp_min;
             $("#highTemp").append("<div>High:"+highList+" Low:"+lowList+"</div>");
         }
+        var indexURL = "http://api.openweathermap.org/data/2.5/uvi?appid=3962360e63ed61d5bc7374f81a485e9b&lat="+response.city.coord.lat+"&lon="+response.city.coord.lon+"";
+         
 
-
-        });
         $.ajax ({
             url:indexURL,
             method:"GET"
-        }).then(function (response){
-            cityLat = response.city.coord.lat;
-            cityLon = response.city.coord.lon;
+        }).then(function (response1){
             
-            var index = response.value
-            $("#uvIndex").html("UV Index: " + index);        
-    
+            
+            
+            var index = response1.value
+            $("#uvIndex").html("UV Index: " + index); 
+            })
+        
+
         });
-    })
-    
+      
+    });
+
+   
     
 
     
@@ -68,4 +65,4 @@ var searchData = $("#searchButton").click(function(){
 
        
    
-    })
+})
